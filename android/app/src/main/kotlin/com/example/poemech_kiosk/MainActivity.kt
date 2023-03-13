1db881e3c430534ad579527f191f94e25ade8e61
hackerFaruk <onuralparslan97@gmail.com>
2023-03-14 02:55:25 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/poemech_kiosk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poemech_kiosk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainPage());
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Selection Maker'),
+        ),
+        body: const Center(child: SelectionButtons()),
+      ),
+    );
+  }
+}
+
+class SelectionButtons extends StatefulWidget {
+  const SelectionButtons({super.key});
+
+  @override
+  State<SelectionButtons> createState() => _SelectionButtonsState();
+}
+
+class _SelectionButtonsState extends State<SelectionButtons> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+            width: 200,
+            height: 200,
+            child: ElevatedButton(
+                onPressed: () {
+                  print('object');
+                },
+                child: const Image(image: AssetImage('images/human.png')))),
+        SizedBox(
+            width: 200,
+            height: 200,
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const DogPage()),
+                  );
+                },
+                child: const Hero(
+                    tag: 'dogpic',
+                    child: Image(image: AssetImage('images/dog.png'))))),
+      ],
+    );
+  }
+}
+
+class DogPage extends StatefulWidget {
+  const DogPage({super.key});
+
+  @override
+  State<DogPage> createState() => _DogPageState();
+}
+
+class _DogPageState extends State<DogPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const BackButton(color: Colors.white),
+        title: const Text('Dog Washy washy'),
+      ),
+      body: Column(
+        children: [
+          const Hero(
+              tag: 'dogpic', child: Image(image: AssetImage('images/dog.png'))),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('go back'))
+        ],
+      ),
+    );
+  }
+}
