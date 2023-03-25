459079fcc4379d24a0434de6c0b54bbebe492d56
hackerFaruk <onuralparslan97@gmail.com>
2023-03-25 15:31:01 +0300

    deliren tuşlar

--- lib/grayableselection.dart
+++ lib/grayableselection.dart
@@ -1,19 +1,17 @@
+import 'dart:ui';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'globals.dart' as global;
 
 class GrayableNine extends StatefulWidget {
   const GrayableNine({super.key});
-
   @override
   State<GrayableNine> createState() => _GrayableNineState();
 }
 
 class _GrayableNineState extends State<GrayableNine> {
   List<int> butArr = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  List<int> isButtonSelected = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  List<int> allOpen = List.unmodifiable([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-  List<int> allGray = List.unmodifiable([1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
   // list.unmodifiable makes list immutable so you wont change those by accident
 
@@ -23,6 +21,10 @@ class _GrayableNineState extends State<GrayableNine> {
   Widget build(BuildContext context) {
     final screnSize = MediaQuery.of(context).size.width;
 
+    void rerefresh() {
+      setState(() {});
+    }
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
@@ -30,56 +32,15 @@ class _GrayableNineState extends State<GrayableNine> {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             // thast the first button
-            SizedBox(
-              width: screnSize * 0.25,
-              height: screnSize * 0.25,
-              child: InkWell(
-                onTap: () {
-                  // eğer buton açıksa
-                  if (butArr[0] == 0) {
-                    // let here be states
-                    if (isButtonSelected[0] == 0) {
-                      // eğer button seçili değilse
-                      isButtonSelected[0] = 1;
-                      global.selected = 'spf15';
-                      setState(() {
-                        butArr = allGray; // makes all gray
-                        butArr[0] = 0; // makes current button open
-                      });
-                    } else if (isButtonSelected[0] == 1) {
-                      // eğer seçili buton tekrar tıklanırsa seçim sıfırmlanır
-                      butArr = allOpen;
-                      global.selected = '';
-                    }
-                  }
-                },
-                child: GreyoutButtons(
-                  grayout: butArr[0],
-                  icon: 'images/spf15.png',
-                ),
-              ),
-            ),
+            RadioButtons(iconNumber: 3, iconImage: 'images/spf15.png'),
             // end of first button
 
             // thast the second button
-            SizedBox(
-              width: screnSize * 0.25,
-              height: screnSize * 0.25,
-              child: InkWell(
-                onTap: () {
-                  // let here be states
-                  setState(() {
-                    butArr = allGray; // makes all gray
-                    butArr[1] = 0; // makes current button open
-                  });
-                },
-                child: GreyoutButtons(
-                  grayout: butArr[1],
-                  icon: 'images/spf30.png',
-                ),
-              ),
-            ),
+            RadioButtons(iconNumber: 1, iconImage: 'images/spf30.png'),
+
             // end of second button
+
+            RadioButtons(iconNumber: 2, iconImage: 'images/spf50.png')
           ],
         ),
         Row(
@@ -204,13 +165,44 @@ class _RadioButtonsState extends State<RadioButtons> {
   @override
   Widget build(BuildContext context) {
     final screnSize = MediaQuery.of(context).size.width;
-
     return // thast the first button
         SizedBox(
       width: screnSize * 0.25,
       height: screnSize * 0.25,
       child: InkWell(
         onTap: () {
+          setState(() {
+            global.butArr = global.butArr[widget.iconNumber] == 1
+                ? global.allOpen
+                : global.allGray;
+          });
+        },
+        child: GreyoutButtons(
+          grayout: global.butArr[widget.iconNumber],
+          icon: widget.iconImage,
+        ),
+      ),
+    );
+    // end of first button
+  }
+}
+
+/// supa cool button mechanism ____^_____^____^____^____^_____^____^_____
+/// 
+/// 
+/// 
+
+
+
+
+
+
+/*
+
+
+    print(global.butArr);
+          print(widget.iconNumber);
+          print(global.butArr[widget.iconNumber]);
           // eğer buton açıksa
           if (global.butArr[widget.iconNumber] == 0) {
             // let here be states
@@ -228,16 +220,12 @@ class _RadioButtonsState extends State<RadioButtons> {
               global.butArr = global.allOpen;
               global.selected = '';
             }
+          } else {
+            setState(() {
+              global.butArr = global.allGray; // makes all gray
+              global.butArr[widget.iconNumber] = 0;
+            });
           }
-        },
-        child: GreyoutButtons(
-          grayout: global.butArr[widget.iconNumber],
-          icon: widget.iconImage,
-        ),
-      ),
-    );
-    // end of first button
-  }
-}
 
-/// supa cool button mechanism ____^_____^____^____^____^_____^____^_____
+
+*/
