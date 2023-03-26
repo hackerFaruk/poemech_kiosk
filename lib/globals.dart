26f8dd1e28e176d8dfaec9b3832d0520691bf65a
hackerFaruk <onuralparslan97@gmail.com>
2023-03-26 23:30:56 +0300

    just wantto make commits

--- lib/globals.dart
+++ lib/globals.dart
@@ -13,7 +13,7 @@ List<int> allOpen = List.unmodifiable([0, 0, 0, 0, 0, 0, 0, 0, 0]);
 List<int> allGray = List.unmodifiable([1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
 // list.unmodifiable makes list immutable so you wont change those by accident
-hi
+
 int but1 = 0;
 int but2 = 0;
 int but3 = 0;
