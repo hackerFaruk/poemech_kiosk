f925cce8dabebc3cf2c1b7e7b42ea9c1ea41d2f1
hackerFaruk <onuralparslan97@gmail.com>
2023-03-26 22:44:52 +0300

    clerad dead code

--- lib/main.dart
+++ lib/main.dart
@@ -149,128 +149,7 @@ class PageBanner extends StatelessWidget {
   }
 }
 
-// Gender Page----------------------------------------------------------
-class GenderSelectionPage extends StatelessWidget {
-  final String language;
-  const GenderSelectionPage({super.key, required this.language});
-
-  @override
-  Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-
-    return Scaffold(
-      appBar: AppBar(
-        leading: const BackButton(
-          color: Colors.white,
-        ),
-        title: const Text('Gender Selection'),
-      ),
-      body: Column(children: [
-        Hero(
-          tag: 'flagPic_$language',
-          child: Container(
-            color: language == 'en'
-                ? const Color.fromARGB(255, 5, 40, 122)
-                : const Color.fromRGBO(227, 10, 23, 1),
-            child: PageBanner(
-                bannerImg: language == "en"
-                    ? 'images/flag_en.png'
-                    : 'images/flag_tr.png'),
-          ),
-        ),
-        Container(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            SizedBox(
-                width: screenSize.width * 0.3,
-                height: screenSize.width * 0.3,
-                child: ElevatedButton(
-                    onPressed: () {
-                      null;
-                    },
-                    child:
-                        const Image(image: AssetImage('images/female.png')))),
-            SizedBox(
-                width: screenSize.width * 0.3,
-                height: screenSize.width * 0.3,
-                child: ElevatedButton(
-                    onPressed: () {
-                      null;
-                    },
-                    child: const Image(image: AssetImage('images/male.png'))))
-          ],
-        )
-      ]),
-    );
-  }
-}
-// Gender Page ^^^^^^^^^^^
-
 // İşlem seçim sayfası ___________ProcessPage_______________selection__________________________
-
-class ProcessPageOld extends StatefulWidget {
-  final String lang;
-  const ProcessPageOld({super.key, required this.lang});
-
-  @override
-  State<ProcessPageOld> createState() => _ProcessPageState();
-}
-
-class _ProcessPageState extends State<ProcessPageOld> {
-  @override
-  Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    String selection = widget.lang == 'en'
-        ? 'Please Select Application'
-        : 'Lütfen İşlem Seçiniz';
-
-    return SizedBox(
-      width: screenSize.width,
-      child: Scaffold(
-        appBar: AppBar(
-          leading: const BackButton(
-            color: Colors.white,
-          ),
-          title: Text(selection),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                SizedBox(
-                  height: 30,
-                ),
-                ThreeButtonRow(
-                  icon1: 'images/spf15.png',
-                  icon2: 'images/spf30.png',
-                  icon3: 'images/spf50.png',
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                ThreeButtonRow(
-                  icon1: 'images/shower.png',
-                  icon2: 'images/moist.png',
-                  icon3: 'images/bronz.png',
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                ThreeButtonRow(
-                  icon1: 'images/change.png',
-                  icon2: 'images/wheel.png',
-                  icon3: 'images/dogbutton.png',
-                )
-              ]),
-        ),
-      ),
-    );
-  }
-}
 
 class ProcessPage extends StatelessWidget {
   final String lang;
@@ -310,154 +189,7 @@ class ProcessPage extends StatelessWidget {
 
 // İşlem Seçim Sayfası ^^^^^^^^^^^^^^^^ process page^^^^^^^^^^^^^^^^ selection^^^^^^^^^^^^^^^^^^^^^^
 
-// Buttons Row _______________________________row of 2 buttons______________________
-
-class ButtonsRow extends StatelessWidget {
-  final String icon1;
-  final String icon2;
-
-  const ButtonsRow({super.key, required this.icon1, required this.icon2});
-
-  @override
-  Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    return Material(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          SizedBox(
-            width: screenSize.width * 0.3,
-            height: screenSize.width * 0.3,
-            child: InkWell(
-              onTap: () {
-                // instead of printing use it ass command
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            StartProcessPage(application: icon1)));
-              },
-              child: Hero(
-                tag: icon1,
-                child: Image(
-                  image: AssetImage(icon1),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            width: screenSize.width * 0.3,
-            height: screenSize.width * 0.3,
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            StartProcessPage(application: icon2)));
-              },
-              child: Hero(
-                tag: icon2,
-                child: Image(
-                  image: AssetImage(icon2),
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-//Buttons Row  ^^^^^^^^^^^^^^^^^^^^^^^^row of 2 buttons^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-// 3button row _________________________________________________-
-
-class ThreeButtonRow extends StatelessWidget {
-  final String icon1;
-  final String icon2;
-  final String icon3;
-
-  const ThreeButtonRow(
-      {super.key,
-      required this.icon1,
-      required this.icon2,
-      required this.icon3});
-
-  @override
-  Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-
-    return Material(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          SizedBox(
-            width: screenSize.width * 0.25,
-            height: screenSize.width * 0.25,
-            child: InkWell(
-              onTap: () {
-                // instead of printing use it ass command
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            StartProcessPage(application: icon1)));
-              },
-              child: Hero(
-                tag: icon1,
-                child: Image(
-                  image: AssetImage(icon1),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            width: screenSize.width * 0.25,
-            height: screenSize.width * 0.25,
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            StartProcessPage(application: icon2)));
-              },
-              child: Hero(
-                tag: icon2,
-                child: Image(
-                  image: AssetImage(icon2),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            width: screenSize.width * 0.25,
-            height: screenSize.width * 0.25,
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            StartProcessPage(application: icon3)));
-              },
-              child: Hero(
-                tag: icon3,
-                child: Image(
-                  image: AssetImage(icon3),
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-// 3button row ^^^^^^^^-^^^^^^^^-^^^^^^^^-^^^^^^^^-^^^^^^^^-^^^^^^^^-
+// string clearing function _______________________________________
 
 clearString(input) {
   // input will be the button icon 'images/img.png' we need img part
@@ -465,6 +197,8 @@ clearString(input) {
   String mem1 = input.replaceAll("images/", "");
   return mem1.substring(0, mem1.indexOf('.'));
 }
+
+// string clearing functiom ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 // StartProcessPage ____________________
 
@@ -597,10 +331,11 @@ class FurSlection extends StatelessWidget {
 
 // fur cıntrıl ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-// Conditional COntrol row
-///üste yaz
+// Conditional Row ____________________________________________________
+
+/// If the application is the dog button, return the FurSlection widget. If the application is the
+/// wheelchair button, return the WheelChairRow widget. Otherwise, return the OKCancelRow widget
 class ConditionalControlRow extends StatelessWidget {
-  /// dog buttonsa farklı
   final String application;
   const ConditionalControlRow({super.key, required this.application});
 
