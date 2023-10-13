04bc45a9ad433b5676ccc89b2e24a346d2381c80
Riis35 <farukozkan56@hotmail.com>
2023-10-13 17:03:53 +0300

    new life new fife

--- lib/emergencyStop.dart
+++ lib/emergencyStop.dart
@@ -1,6 +1,11 @@
 // ignore: file_names
+import 'dart:convert';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'emergencyPage.dart' as emergency;
+import 'package:http/http.dart' as http;
+import 'processcontrol.dart' as process;
 
 // write  staefulW for stateful widget and rename
 
@@ -19,6 +24,9 @@ class _emergencyStopState extends State<emergencyStop> {
       child: Center(
         child: InkWell(
             onTap: () {
+              process.ProcessControlPage.music.stop();
+              process.ProcessControlPage.music.play(AssetSource("alarm.mp3"));
+              emergencyMail();
               Navigator.push(
                 context,
                 MaterialPageRoute(
@@ -30,5 +38,30 @@ class _emergencyStopState extends State<emergencyStop> {
                 child: Image(image: AssetImage('images/emg2.png')))),
       ),
     );
+  }
+
+  void emergencyMail() async {
+    bool connection = true;
+
+    var url = Uri.parse("https://poemech.com.tr:3001/api/mail/emergencyButton");
+    final body = json.encode({"id": "AB010723/01", "mail": "info@onarfa.com"});
+
+    // ignore: prefer_typing_uninitialized_variables
+    var res;
+    try {
+      res = await http.post(url,
+          headers: {"Content-Type": "application/json"}, body: body);
+      // ignore: unused_catch_clause
+    } on Exception catch (e) {
+      //print(e.toString());
+      connection = false;
+    }
+    if (connection) {
+      final Map<String, dynamic> data = json.decode(res.body);
+
+      if (data['done'] == 'false') {
+      } else if (data['done'] == 'true') {
+      } else {}
+    }
   }
 }
